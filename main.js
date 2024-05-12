fb9eedbb39a50af649ebe24483513219a5dd2078
ralphdosreis <ralphdosreistorres@gmail.com>
2024-05-12 11:16:19 -0300

    interacao-login realizada

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{var e=document.getElementById("container"),t=document.getElementById("register"),n=document.getElementById("login");t.addEventListener("click",(function(){e.classList.add("active")})),n.addEventListener("click",(function(){e.classList.remove("active")}))})();
